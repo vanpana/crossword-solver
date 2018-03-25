f6251b58a069c007d4ae7646648ccc4eef0658db
vanpana <van.panaite@gmail.com>
2018-03-25 14:33:06 +0300

    computed locations

--- src/Point.py
+++ src/Point.py
@@ -10,3 +10,23 @@ class Point:
         if self.x == other.x:
             return abs(self.y - other.y)
         return abs(self.x - other.x)
+
+    def between(self, p1, p2):
+        if p1 == p2:
+            return False
+
+        if self.x == p1.x or self.x == p2.x:
+            if p1.y < p2.y:
+                return p1.y <= self.y <= p2.y
+            return p2.y <= self.y <= p1.y
+
+        if self.y == p1.y or self.y == p2.y:
+            if p1.x < p2.x:
+                return p1.x <= self.x <= p2.x
+            return p2.x <= self.x <= p1.x
+
+    def __str__(self):
+        return "{0}, {1}".format(self.x, self.y)
+
+    def __repr__(self):
+        return str(self)
--- src/Problem.py
+++ src/Problem.py
@@ -1,7 +1,10 @@
+from Point import Point
+
+
 class Problem:
     def __init__(self, filename):
         self.matrix = []
-        self.location = None  # each item will the starting point and end point of the empty space
+        self.location = []  # each item will the starting point and end point of the empty space
         self.words = []
         self.filename = filename
 
@@ -20,6 +23,57 @@ class Problem:
             if len(line) == 1:
                 k = int(line[0])
             elif len(line) == k:
-                self.matrix.append(line)
+                self.matrix.append([0 if element == '0' else '#' for element in line])
             else:
                 self.words = [word for word in line]
+
+        self.location.extend(self.__fill_location_horizontal())
+        self.location.extend(self.__fill_location_vertical())
+
+    def __fill_location_horizontal(self):
+        location = []
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix)):
+                if len(location) == 0:
+                    process = True
+                else:
+                    process = not Point(i, j).between(location[-1][0], location[-1][1])
+
+                if self.matrix[i][j] == 0 and process:
+                    offset = 0
+                    while self.matrix[i][j + offset] == 0 and j + offset < len(self.matrix):
+                        offset += 1
+                        if j + offset == len(self.matrix):
+                            break
+
+                    if offset != 1:
+                        offset -= 1
+                        location.append((Point(i, j), Point(i, j + offset)))
+
+                    if j + offset + 1 == len(self.matrix):
+                        break
+        return location
+
+    def __fill_location_vertical(self):
+        location = []
+        for j in range(len(self.matrix)):
+            for i in range(len(self.matrix)):
+                if len(location) == 0:
+                    process = True
+                else:
+                    process = not Point(i, j).between(location[-1][0], location[-1][1])
+
+                if self.matrix[i][j] == 0 and process:
+                    offset = 0
+                    while self.matrix[i + offset][j] == 0 and i + offset < len(self.matrix):
+                        offset += 1
+                        if i + offset == len(self.matrix):
+                            break
+
+                    if offset != 1:
+                        offset -= 1
+                        location.append((Point(i, j), Point(i + offset, j)))
+
+                    if i + offset + 1 == len(self.matrix):
+                        break
+        return location
